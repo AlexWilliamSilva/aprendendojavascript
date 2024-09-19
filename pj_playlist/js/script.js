406afdd0435c8aa472b1a338b50a22b6d4bdf696
alex <alexwilliamgomessilva168@gmail.com>
2024-09-19 17:21:46 -0300

    19/09 commit push - avançando ainda mais no javascript

--- pj_playlist/js/script.js
+++ pj_playlist/js/script.js
@@ -5,6 +5,7 @@ const cover = document.getElementById("cover");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
 const previous = document.getElementById("previous");
+const currentProgress = document.getElementById("current-progress")
 
 //seção de variáveis auxiliares para o meu código
 const enemy = {
@@ -23,45 +24,14 @@ let isPlaying = false;
 const playlist = [enemy, sharks];
 let index = 0;
 
+/* inicializando a musica */
 function initializeSong() {
-    cover.src = `/IMAGEM/${playlist[currentSong].file}.png`;
-    songName.innerText = playlist[currentSong].songName;
-    bandName.innerText = playlist[currentSong].artist;
-    song.src = `/songs/${playlist[currentSong].file}.mp3`; 
-}
-
-initializeSong();
-
-function previousSong() {
-    if(index === 0){
-        index = playlist.length - 1;
-    }
-    else {
-        index -=1;
-    }
+    cover.src = `/IMAGEM/${playlist[index].file}.png`;
+    song.src = `/songs/${playlist[index].file}.mp3`; 
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
 }
 initializeSong();
-playSong();
-
-function nextSong() {
-    if(index === playlist.length - 1;) {
-        index = 0;
-    }
-    else {
-        index +=1;
-    }
-}
-initializeSong();
-playSong();
-
-
-
-
-
-
-/*
-songName.innerText = 'Enemy - Imagine dragons';
-//let isPlaying = false;
 
 function playSong() {
     play.querySelector(".bi").classList.remove("bi-play-circle-fill");
@@ -74,10 +44,8 @@ function pauseSong() {
     play.querySelector(".bi").classList.add("bi-play-circle-fill");
     play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
     song.pause();
-   // isPlaying = false;
-}
-
- play.addEventListener("click", playSong); <---- cod retirado  
+    isPlaying = false;
+} 
 
 function playPauseDecider() {
     if (isPlaying === true) {
@@ -85,4 +53,41 @@ function playPauseDecider() {
     } else {
       playSong();
     }
-}*/
+}
+
+function previousSong() {
+    if(index === 0){
+        index = playlist.length - 1;
+    }
+    else {
+        index -=1;
+    }
+    initializeSong();
+    playSong();
+}
+
+
+function nextSong() {
+    if(index === playlist.length - 1) {
+        index = 0;
+    }
+    else {
+        index +=1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function updateProgressBar(){
+    /*song.currentTime
+    song.duration*/
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+initializeSong();
+
+// possivel mudança para depois
+play.addEventListener("click", playPauseDecider);
+next.addEventListener("click", nextSong);
+previous.addEventListener("click", previousSong);
+song.addEventListener('timeupdate', updateProgressBar);
