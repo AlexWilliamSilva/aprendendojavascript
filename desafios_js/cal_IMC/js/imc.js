ff7c1178e0446c997905604139d9d488526abdcc
alex <alexwilliamgomessilva168@gmail.com>
2024-09-26 17:25:30 -0300

    26/09 - salvando o desafio do imc

---
+++ desafios_js/cal_IMC/js/imc.js
@@ -0,0 +1,38 @@
+const nome = document.getElementById("nome");
+const altura = document.getElementById("altura");
+const peso = document.getElementById("peso");
+const result = document.getElementById("result");
+
+function calculoIMC(event) {
+    event.preventDefault();
+
+    const pesoValor = parseFloat(peso.value);
+    const alturaValor = parseFloat(altura.value);
+
+    if (isNaN(pesoValor) || isNaN(alturaValor) || alturaValor <= 0 || pesoValor <= 0) {
+        result.valor = "Insira valores validos para o cálculo";
+        return;
+    }
+
+    var imc = pesoValor / (alturaValor * alturaValor);
+    imc = imc.toFixed(2);
+
+    if (imc < 18.5) {
+        result.value = `Seu peso é de ${imc} e você está abaixo do peso`;
+    }
+    else if(imc >= 18.6 && imc <= 24.9) {
+        result.value = `Seu peso é de ${imc} e você está com um peso normal`;
+    }
+    else if(imc >= 25 && imc <= 29.9) {
+        result.value = `Seu peso é de ${imc} e você está com sobrepeso`;
+    }
+    else if(imc >= 30 && imc <= 34.9) {
+        result.value = `Seu peso é de ${imc} e você está com obesidade grau I`;
+    }
+    else if(imc >= 35 && imc <= 39.9) {
+        result.value = `Seu peso é de ${imc} e você está com obesidade grau II`;
+    }
+    else {
+        result.value = `Seu peso é de ${imc} e você está com obesidade grau III ou extrema`;
+    }
+}
